8a1269ece0c3e8ebfb940bf10c4c38c475dd29a6
Andre <vtavares.andre@gmail.com>
2021-04-22 19:17:11 -0300

    add homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react"
+import AvatarUpload from "../../components/AvatarUpload"
+import { useState } from "react"
+import { Container, Grid, Box } from "@material-ui/core"
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            
-        </div>
-    )
+	const [croppedImage, setCroppedImage] = useState(null)
+	return (
+		<Container maxWidth='sm'>
+			<Box pt={5}>
+				<AvatarUpload setCroppedImage={setCroppedImage}  />
+			</Box>
+			<Box>
+				<h3> Parent Component </h3>
+				<img alt='Cropped Image' src={croppedImage} />
+			</Box>
+		</Container>
+	)
 }
 
 export default Home
